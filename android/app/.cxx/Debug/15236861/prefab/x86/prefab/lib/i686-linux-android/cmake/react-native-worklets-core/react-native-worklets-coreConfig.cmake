b6fc102e6fa7346391c54610ec4d9f63a930ab00
Ali Mansour <amansour@rapide.om>
2025-12-17 13:52:15 +0200

    chore: remove .cxx build artifacts from version control

--- android/app/.cxx/Debug/15236861/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-worklets-core/react-native-worklets-coreConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-worklets-core::rnworklets)
-add_library(react-native-worklets-core::rnworklets SHARED IMPORTED)
-set_target_properties(react-native-worklets-core::rnworklets PROPERTIES
-    IMPORTED_LOCATION "/home/alialhadimansour/Projects/rapide/navigator-app/node_modules/react-native-worklets-core/android/build/intermediates/cxx/Debug/q6t2k4w5/obj/x86/librnworklets.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/alialhadimansour/Projects/rapide/navigator-app/node_modules/react-native-worklets-core/android/build/headers/rnworklets"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
